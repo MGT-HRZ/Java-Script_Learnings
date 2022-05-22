d972c5335004c084dd6206ccb921bd90d85be4eb
MGT-HRZ <62929236+MGT-HRZ@users.noreply.github.com>
2022-05-22 23:30:33 +0800

    Add files via upload

---
+++ OnMouseOver & OnMouseOut/script.js
@@ -0,0 +1,10 @@
+    
+// Step 2:
+    function MouseOver() {
+        document.getElementById("B1").src = ("book1.png");
+    }
+
+// Step 3:
+    function MouseOut() {
+        document.getElementById("B1").src = ("book2.png");
+    }
